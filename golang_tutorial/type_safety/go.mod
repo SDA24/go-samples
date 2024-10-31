60fadfe42cd094d8e9127e3e6cfb82aaa1860e1f
saeed dehghani ashkezari <saeed.dehghani88@gmail.com>
2024-11-01 03:16:11 +0330

    casting float64 to int

---
+++ golang_tutorial/type_safety/go.mod
@@ -0,0 +1,3 @@
+module type_safety
+
+go 1.22.2
---
+++ golang_tutorial/type_safety/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	var (
+		a = 2
+		b = 3.01
+	)
+
+	fmt.Printf("a: %8T , %[1]v\n", a)
+	fmt.Printf("b: %8T , %[1]v\n", b)
+
+	// a = b error
+	a = int(b)
+
+	fmt.Printf("a: %8T , %[1]v\n", a)
+}
